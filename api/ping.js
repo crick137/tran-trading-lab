aba5516fc163e9bee305ad8f7f4ecc4a272ba0c8
rick137 <izuowangdaozi@gmail.com>
2025-10-18 22:28:26 +0800

    chore: upgrade runtime to nodejs22.x for Vercel

--- api/ping.js
+++ api/ping.js
@@ -1,4 +1,4 @@
-export const config = { runtime: 'nodejs18.x' };
+export const config = { runtime: 'nodejs22.x' };
 
 export default async function handler(req, res) {
   res.setHeader('content-type', 'application/json; charset=utf-8');
