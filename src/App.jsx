16a83cf489a81854a90bc500d6b7aed092250f4b
crick137 <izuowangdaozi@gmail.com>
2025-12-29 19:53:33 +0800

    refactor: Replace AdminPage with ContentLibrary
    - Delete old 80KB AdminPage.jsx
    - Create ContentLibrary.jsx - beautiful gallery for articles & tools
    - Change route /admin to /library
    - Reduces codebase size significantly

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import { NotificationCenter } from './components/NotificationCenter'
 
 // 路由懒加载
 const TranTradingTerminal = lazy(() => import('./components/TranTradingTerminal'))
-const AdminPage = lazy(() => import('./pages/AdminPage'))
+const ContentLibrary = lazy(() => import('./pages/ContentLibrary'))
 
 // 独立可分享页面 - 使用命名导出
 const KellyArticlePage = lazy(() => import('./pages/KellyPages').then(m => ({ default: m.KellyArticlePage })))
@@ -25,7 +25,7 @@ function App() {
                             <Routes>
                                 <Route path="/" element={<TranTradingTerminal />} />
                                 <Route path="/analysis/:articleId" element={<TranTradingTerminal initialView="article-detail" />} />
-                                <Route path="/admin" element={<AdminPage />} />
+                                <Route path="/library" element={<ContentLibrary />} />
 
                                 {/* 可独立分享的文章和工具页面 */}
                                 <Route path="/article/kelly-criterion" element={<KellyArticlePage />} />
